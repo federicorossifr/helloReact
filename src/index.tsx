b8a6b66c3f2ced5bea81784b4e8af58ebfe595ef
Federico Rossi <federico.rossi@phd.unipi.it>
2021-10-24 10:14:00 +0200

    Setup for a dev live server

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useReducer } from "react";
 import ReactDOM from "react-dom";
 
+const counterReducer = (state,action) => {
+    switch(action.type) {
+        case 'inc': return state + 1;
+        case 'dec': return state - 1;
+        case 'set': return action.value;
+    }
+}
+
+const App = ({appTitle}) => {
+    const [counter,dispatch] = useReducer(counterReducer,0);
+    const [valueInput,setValueInput] = useState(0)
+    return (
+        <>
+            <h1>{appTitle}</h1>
+            <button onClick={() => dispatch({type:'inc'}) }>+</button>
+            <p>{counter}</p>
+            <button onClick={() => dispatch({type:'dec'}) }>-</button>
+            <hr></hr>
+            <input value={valueInput} onChange={(e) => setValueInput(parseInt(e.target.value))} type="number"></input>
+            <button onClick={() => dispatch({type:'set',value:valueInput}) }>Set</button>
+
+        </>
+    )
+}
+
 ReactDOM.render(
-    <div>It works?</div>,
+    <App appTitle="Hello!" />,
     document.getElementById("hello-container")
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,50 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const  webpack = require('webpack');
 module.exports = {
-  // Where files should be sent once they are bundled
- entry: {
-     index: "/src/index.tsx"
- },
- output: {
-   path: path.join(__dirname, '/dist/js'),
-   filename: '[name].js'
- },
- resolve: {
-  extensions: ['.ts', '.tsx', '.js']
-},
+  mode: 'development',
+  entry: {
+    index: "/src/index.tsx"
+  },
+  output: {
+    path: path.join(__dirname, '/dist/js'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  devServer: {
+    compress: true,
+    hot: true,
+    host: '0.0.0.0',
+    port: 3000,
+  },
+
 
   // Rules of how webpack will take our files, complie & bundle them for the browser 
- module: {
-   rules: [
-     {
-       test: /\.(js|jsx|ts|tsx)$/,
-       exclude: /nodeModules/,
-       use: {
-         loader: 'babel-loader'
-       }
-     },
-     {
-       test: /\.css$/,
-       use: ['style-loader', 'css-loader']
-     }
-   ]
- }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 }
